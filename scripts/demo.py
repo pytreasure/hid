7ddec7df32cefc7ae559d7302b8cc821962a5bf5
hunzsig <mzyhaohaoren@qq.com>
2020-05-27 00:18:53 +0800

    up

--- scripts/demo.py
+++ scripts/demo.py
@@ -7,5 +7,9 @@ from lib import hid
 
 def run(com):  # 跑脚本
     hid.set_com(com)  # 设置端口
+    once = 10
     while True:  # 死循环
-        hid.keyboard("A", 0)
+        once -= 1
+        if once < 0:
+            break
+        hid.keyboard("UP", 0)
