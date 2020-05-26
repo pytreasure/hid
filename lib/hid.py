17eb5cfaff893fe03ef107293231f109c0266834
hunzsig <mzyhaohaoren@qq.com>
2020-05-26 17:42:24 +0800

    up

--- lib/hid.py
+++ lib/hid.py
@@ -1,29 +1,33 @@
 # Core code of HID.
 # Build commands for HID keyboard and mouse.
+import time
+import keyword
 
-hidCom = None
+hid_com = None
 
 
 def set_com(which_com):
-    global hidCom
-    hidCom = which_com
+    global hid_com
+    hid_com = which_com
 
 
 def keyboard(keys, delay):
-    if hidCom is None:
+    global hid_com
+    if hid_com is None:
         return
     commands = {
 
     }
     keys_type = type(keys)
-    if keys_type == "number":
-        keys_type = str(keys_type)
-    if keys_type == "list" or keys_type == "tuple":
-        keys_type = 1
-    elif keys_type == "string":
+    if keys_type == "string":
         keys_type = 2
-    else:
-        return ()
+    elif keys_type == "number":
+        keys = str(keys)
+    elif keys_type == "list" or keys_type == "tuple":
+        keys_type = 1
+    put = bytes([0x57, 0XAB, 0X00, 0X02, 0X08, 0X00, 0X00, 0X04, 0X00, 0X00, 0X00, 0X00, 0X00, 0X10])
+    hid_com.write(put)
+    time.sleep(delay)
 
 
 def mouse(keys):
