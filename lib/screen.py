c1afd3b69ad5e9e8301e9fa2d3ea5fbc914170d8
hunzsig <mayhaohaoren@qq.com>
2020-05-31 09:17:53 +0800

    up

---
+++ lib/screen.py
@@ -0,0 +1,15 @@
+import win32gui
+
+hwnd_title = dict()
+
+
+def get_all_hwnd(hwnd, mouse):
+    if win32gui.IsWindow(hwnd) and win32gui.IsWindowEnabled(hwnd) and win32gui.IsWindowVisible(hwnd):
+        hwnd_title.update({hwnd: win32gui.GetWindowText(hwnd)})
+
+
+win32gui.EnumWindows(get_all_hwnd, 0)
+
+for h, t in hwnd_title.items():
+    if t is not "":
+        print(h, t)
